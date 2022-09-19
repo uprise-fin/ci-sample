bc7f7a3fb92713dd6f9735a8f75f028618435060
qwerty <qwerty@heybit.io>
2022-09-19 17:38:07 +0900

    chore: add /env endpoint

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from os import getenv
 
 app = Flask(__name__)
 
@@ -6,6 +7,10 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def root():
     return "Hello World! - from Flask"
+
+@app.route("/env", methods=["GET"])
+def loadenv():
+    return getenv("ENV", "NONE")
 
 
 if __name__ == "__main__":
